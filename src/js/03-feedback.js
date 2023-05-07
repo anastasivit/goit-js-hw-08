711c2e35bce99392facbb8d04708d84d6e194cb6
anastasivit <119945000+anastasivit@users.noreply.github.com>
2023-05-07 12:05:14 +0300

    Add 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
 
+const saveFormState = () => {
+  const emailInput = document.querySelector('input[name="email"]');
+  const messageInput = document.querySelector('textarea[name="message"]');
+  const feedbackFormState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify(feedbackFormState)
+  );
+};
+
+document
+  .querySelector('.feedback-form')
+  .addEventListener('input', throttle(saveFormState, 500));
+
+const feedbackFormState = JSON.parse(
+  localStorage.getItem('feedback-form-state')
+);
+
+if (feedbackFormState) {
+  document.querySelector('input[name="email"]').value = feedbackFormState.email;
+  document.querySelector('textarea[name="message"]').value =
+    feedbackFormState.message;
+}
+
+document.querySelector('.feedback-form').addEventListener('submit', event => {
+  event.preventDefault();
+  localStorage.removeItem('feedback-form-state');
+  document.querySelector('input[name="email"]').value = '';
+  document.querySelector('textarea[name="message"]').value = '';
+  const feedbackFormState = {
+    email: '',
+    message: '',
+  };
+  console.log(feedbackFormState);
+});
