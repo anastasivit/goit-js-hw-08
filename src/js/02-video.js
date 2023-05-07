6ead5aa45d33ea9350547ba3d47c2b01497e9816
anastasivit <119945000+anastasivit@users.noreply.github.com>
2023-05-07 12:03:14 +0300

    Add 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import Vimeo from '@vimeo/player/dist/player.js';
+import throttle from 'lodash.throttle';
 
+const player = new Vimeo(document.querySelector('#vimeo-player'));
+const currentTimeKey = 'videoplayer-current-time';
+
+const saveCurrentTime = time => {
+  localStorage.setItem(currentTimeKey, time);
+};
+
+const getSavedTime = () => {
+  return localStorage.getItem(currentTimeKey);
+};
+
+player.on(
+  'timeupdate',
+  throttle(() => {
+    player.getCurrentTime().then(seconds => {
+      saveCurrentTime(seconds);
+    });
+  }, 1000)
+);
+
+const savedTime = getSavedTime();
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
